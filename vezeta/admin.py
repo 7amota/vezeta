e06f54d9ba6027f7b05b181dda9fbc31fbe36221
7amota <bm993508@gmail.com>
2023-04-21 19:37:37 +0200

    vezeta2

--- vezeta/admin.py
+++ vezeta/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Profile)
+class ProfileModel(admin.ModelAdmin):
+    model = Profile
+    list_display = ('id','name')
+    
+admin.site.register(Profile,ProfileModel)
 admin.site.register(NewPost)
 admin.site.register(Appointmentdef)
